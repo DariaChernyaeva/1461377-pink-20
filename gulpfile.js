ef6e5c1a12efa91c9d4f0c9493a85e9621b7f33e
DariaChernyaeva <65214333+DariaChernyaeva@users.noreply.github.com>
2020-08-30 17:47:50 +0500

    “gulp”

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require('gulp-csso');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
 
 // Styles
 
@@ -16,8 +22,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -49,3 +57,76 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+//Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimatizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75,progressive: true}),
+      imagemin.svgo()
+    ]))
+}
+
+  exports.images = images;
+
+  //Webp
+
+  const webpimages = () => {
+    return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp())
+    .pipe(gulp.dest("source/img"))
+  }
+
+  exports.webpimages = webpimages;
+
+  //Sprite
+
+ const sprite = () => {
+    return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+  }
+
+  exports.sprite = sprite;
+
+
+//Del
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+  //Copy
+
+  const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+      base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+
+
+//Build start
+
+const build = (done) => gulp.series(
+  "clean",
+  "copy",
+  "styles",
+  "sprite")
+  (done);
+
+exports.build = build;
