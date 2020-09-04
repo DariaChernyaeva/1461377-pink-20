92f72f5b7ec6d7ce0ca32034a82337dde41c86b6
DariaChernyaeva <65214333+DariaChernyaeva@users.noreply.github.com>
2020-09-04 22:01:59 +0500

    “menu”

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,7 @@
 var navigation = document.querySelector('.main-navigation');
 var toggle = document.querySelector('.page-header__toggle');
+var cross = document.querySelector('.main-navigation__cross');
+var burger = document.querySelector('.main-navigation__burger');
 
 
 navigation.classList.remove('main-navigation--nojs');
@@ -12,4 +14,5 @@ toggle.addEventListener('click', function(evt) {
   evt.preventDefault();
   navigation.classList.toggle('main-navigation--closed');
   navigation.classList.toggle('main-navigation--opened');
+  toggle.classList.toggle('main-navigation--opened');
 });
