cb5f01ba584c8c136761c83afd78a749fa3337b1
DariaChernyaeva <65214333+DariaChernyaeva@users.noreply.github.com>
2020-09-08 23:21:38 +0500

    “piksell”

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,8 @@
 var navigation = document.querySelector('.main-navigation');
 var toggle = document.querySelector('.page-header__toggle');
 
+var pageheader = document.querySelector('.page-header')
+
 navigation.classList.remove('main-navigation--nojs');
 toggle.classList.remove('page-header__toggle-nojs');
 navigation.classList.remove('main-navigation--opened');
@@ -9,6 +11,7 @@ navigation.classList.add('main-navigation--closed');
 
 toggle.addEventListener('click', function(evt) {
   evt.preventDefault();
+  pageheader.classList.toggle('page-header--opened')
   navigation.classList.toggle('main-navigation--closed');
   navigation.classList.toggle('main-navigation--opened');
   toggle.classList.toggle('main-navigation--opened');
