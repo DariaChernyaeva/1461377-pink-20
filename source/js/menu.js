1dd38fef59760a55a590f157b6f0ad92c07a41d1
DariaChernyaeva <65214333+DariaChernyaeva@users.noreply.github.com>
2020-09-11 14:21:29 +0500

    “ispravleniya”

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,12 @@
 var navigation = document.querySelector('.main-navigation');
 var toggle = document.querySelector('.page-header__toggle');
-
 var pageheader = document.querySelector('.page-header')
 
 navigation.classList.remove('main-navigation--nojs');
 toggle.classList.remove('page-header__toggle-nojs');
 navigation.classList.remove('main-navigation--opened');
 navigation.classList.add('main-navigation--closed');
-
+pageheader.classList.remove('page-header--nojs');
 
 toggle.addEventListener('click', function(evt) {
   evt.preventDefault();
